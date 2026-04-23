e8b922eb9fa1a7eed838eb83d2c4bae440f6af1c
Rohan-Thoma <rohanvailalathoma@gmail.com>
2026-04-23 15:49:09 +0000

    simple llm app in traditonal langchain style

---
+++ 1-Simple-LLM-app.py
@@ -0,0 +1,24 @@
+#Here we are looking at traditonal way of working with langchain
+from langchain_classic.llms import OpenAI
+from langchain_core.prompts import PromptTemplate
+
+#Initialize the LLM
+llm = OpenAI(model_name= 'gpt-4', temperature=0.7)
+
+#Create a Prompt template
+prompt = PromptTemplate(
+    template='suggest a catchy blog title about {topic}',
+    input_variables=['topic']
+)
+
+#Define the input
+topic = input('Enter a topic for the blog : ')
+
+#Format the prompt manually using prompt template
+formatted_prompt = prompt.format(topic=topic)
+
+# Call the LLM directly
+blog_title = llm.predict(formatted_prompt)
+
+# Print the output
+print("Generated Blog Title : ", blog_title)
